bb53d6bf43e888b85023a1ac120dd0a64aa70839
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-13 20:55:19 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace CANestedTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine(employee.EmployeeInsurance.CompanyName);
+
+            Console.ReadKey();  
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+
+        public Insurance EmployeeInsurance { get; set; }
+
+        public Employee() => EmployeeInsurance = new Insurance() { Id = 1, CompanyName = "Test", };
+            
+                
+        public class Insurance
+        {
+            public int Id { get; set; }
+            public string CompanyName { get; set; }
+        }
+
+    }
+    
+    class Department
+    {
+        public int Id { get; set; }
+        public string DepName { get; set; }
+    }
+
+
+}
